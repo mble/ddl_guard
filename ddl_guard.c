e3bf10f5ab27cf3bf4cb05458113b32e6081d12a
Matthew Blewitt <blewitt.matt@gmail.com>
2024-06-10 00:19:52 +0100

    pgindent is so ugly

--- ddl_guard.c
+++ ddl_guard.c
@@ -7,42 +7,41 @@
 PG_MODULE_MAGIC;
 #endif
 
-void _PG_init(void);
-void _PG_fini(void);
-static void ddl_guard_check_inner(EventTriggerData *trigdata);
+void		_PG_init(void);
+void		_PG_fini(void);
 
 static bool ddl_guard_enabled = false;
 
-void _PG_init(void) {
-  DefineCustomBoolVariable("ddl_guard.enabled", "Enable or disable DDL Guard",
-                           NULL, &ddl_guard_enabled, false, PGC_SUSET, 0, NULL,
-                           NULL, NULL);
-}
-
-void _PG_fini(void) { /* Nothing to cleanup */ }
-
-static void ddl_guard_check_inner(EventTriggerData *trigdata) {
-  if (ddl_guard_enabled && !superuser()) {
-    ereport(ERROR,
-            (errcode(ERRCODE_INSUFFICIENT_PRIVILEGE),
-             errmsg("Non-superusers are not allowed to execute DDL statements"),
-             errhint("ddl_guard.enabled is set.")));
-  }
-}
-
 PG_FUNCTION_INFO_V1(ddl_guard_check);
 
-Datum ddl_guard_check(PG_FUNCTION_ARGS) {
-  EventTriggerData *trigdata;
+void
+_PG_init(void)
+{
+	DefineCustomBoolVariable("ddl_guard.enabled", "Enable or disable DDL Guard",
+							 NULL, &ddl_guard_enabled, false, PGC_SUSET, 0, NULL,
+							 NULL, NULL);
+}
 
-  trigdata = (EventTriggerData *)fcinfo->context;
+void
+_PG_fini(void)
+{								/* Nothing to cleanup */
+}
 
-  if (!CALLED_AS_EVENT_TRIGGER(fcinfo))
-    ereport(ERROR,
-            (errcode(ERRCODE_INTERNAL_ERROR),
-             errmsg("ddl_guard_check: not fired by event trigger manager")));
+Datum
+ddl_guard_check(PG_FUNCTION_ARGS)
+{
+	if (!CALLED_AS_EVENT_TRIGGER(fcinfo))
+		ereport(ERROR,
+				(errcode(ERRCODE_INTERNAL_ERROR),
+				 errmsg("ddl_guard_check: not fired by event trigger manager")));
 
-  ddl_guard_check_inner(trigdata);
+	if (ddl_guard_enabled && !superuser())
+	{
+		ereport(ERROR,
+				(errcode(ERRCODE_INSUFFICIENT_PRIVILEGE),
+				 errmsg("Non-superusers are not allowed to execute DDL statements"),
+				 errhint("ddl_guard.enabled is set.")));
+	};
 
-  PG_RETURN_VOID();
+	PG_RETURN_VOID();
 }
