008acc2a0b12fa5f98af03f53f510108f70d4bb2
Matthew Blewitt <blewitt.matt@gmail.com>
2024-06-13 01:00:47 +0100

    feat: fully flesh out lo_sentinel
    lo_sentinel mode now infers if a large object has been created,
    destroyed or modified through one of two ways: spying on
    `pg_largeobject` or spying on `lo*` and `be_lo*` function calls to set
    the sentinel file. I don't expect fully coverage, but it should be "good
    enough".
    
    This is done through an `object_access_hook`, which is exceptionally
    useful for this type of thing.

--- ddl_guard.c
+++ ddl_guard.c
@@ -1,10 +1,13 @@
 #include "postgres.h"
 #include "fmgr.h"
+#include "utils/fmgrtab.h"
 #include "storage/fd.h"
 #include "utils/guc.h"
 #include "commands/event_trigger.h"
 #include "miscadmin.h"
 #include "pgstat.h"
+#include "catalog/objectaccess.h"
+#include "catalog/pg_largeobject.h"
 
 #include <stdio.h>
 #include <unistd.h>
@@ -20,39 +23,53 @@ void		_PG_init(void);
 void		_PG_fini(void);
 void		write_sentinel_file(const char *filename);
 Datum		ddl_guard_check(PG_FUNCTION_ARGS);
-Datum		lo_guard_check(PG_FUNCTION_ARGS);
+static void lob_object_access_hook(ObjectAccessType access, Oid classId, Oid objectId, int subId, void *arg);
 
 static bool ddl_guard_enabled = false;
 static bool ddl_guard_ddl_sentinel = false;
 static bool ddl_guard_lo_sentinel = false;
+static object_access_hook_type next_object_access_hook = NULL;
+
+static const char *large_object_funcs[] = {
+	"lo_create",
+	"lo_creat",
+	"lo_truncate",
+	"lo_truncate64",
+	"lo_unlink",
+	"lowrite",
+	"lo_from_bytea",
+	"be_lowrite",
+	"be_lo_create",
+	"be_lo_creat",
+	"be_lo_unlink",
+	"be_lo_truncate",
+	"be_lo_from_bytea",
+	"be_lo_truncate64",
+	"be_lo_import",
+	"be_lo_import_with_oid",
+};
+static const int lobject_funcs_count = sizeof(large_object_funcs) / sizeof(large_object_funcs[0]);
+static Oid *lobject_func_oids;
+static int	max_reserved_oid = 0;
+static int	min_reserved_oid = 9000;
 
 PG_FUNCTION_INFO_V1(ddl_guard_check);
-PG_FUNCTION_INFO_V1(lo_guard_check);
 
-void
-_PG_init(void)
+/* regards, tom lane */
+static const FmgrBuiltin *
+fmgr_lookupByName(const char *name)
 {
-	DefineCustomBoolVariable("ddl_guard.enabled", "Enable or disable DDL Guard",
-							 NULL, &ddl_guard_enabled, false, PGC_SUSET, 0, NULL,
-							 NULL, NULL);
+	int			i;
 
-	DefineCustomBoolVariable("ddl_guard.ddl_sentinel", "Write sentinel file for DDL statements",
-							 NULL, &ddl_guard_ddl_sentinel, false, PGC_SUSET, 0, NULL,
-							 NULL, NULL);
+	for (i = 0; i < fmgr_nbuiltins; i++)
+	{
+		/* switched to strncmp */
+		/* current max len is 22 in large_object_funcs */
+		if (strncmp(name, fmgr_builtins[i].funcName, 22) == 0)
+			return fmgr_builtins + i;
+	}
 
-	DefineCustomBoolVariable("ddl_guard.lo_sentinel", "Write sentinel file for pg_largeobject modifications",
-							 NULL, &ddl_guard_lo_sentinel, false, PGC_SUSET, 0, NULL,
-							 NULL, NULL);
-
-	unlink(DDL_SENTINEL_FILE);
-	unlink(LO_SENTINEL_FILE);
-
-	EmitWarningsOnPlaceholders("ddl_guard");
-}
-
-void
-_PG_fini(void)
-{								/* Nothing to cleanup */
+	return NULL;
 }
 
 void
@@ -70,6 +87,91 @@ write_sentinel_file(const char *filename)
 		ereport(ERROR,
 				(errcode(ERRCODE_INTERNAL_ERROR),
 				 errmsg("could not write sentinel file \"%s\": %m", filename)));
+}
+
+static bool
+set_lobject_func_oids()
+{
+	const FmgrBuiltin *entry;
+	int			i;
+
+	lobject_func_oids = (Oid *) malloc(lobject_funcs_count * sizeof(Oid));
+	if (lobject_func_oids == NULL)
+	{
+		return false;
+	}
+
+	for (i = 0; i < lobject_funcs_count; i++)
+	{
+		if ((entry = fmgr_lookupByName(large_object_funcs[i])) != NULL)
+		{
+			lobject_func_oids[i] = entry->foid;
+			if (entry->foid < min_reserved_oid)
+			{
+				min_reserved_oid = entry->foid;
+			}
+			else if (entry->foid > max_reserved_oid)
+			{
+				max_reserved_oid = entry->foid;
+			}
+		}
+	}
+
+	return true;
+}
+
+static void
+lob_object_access_hook(ObjectAccessType access, Oid classId, Oid objectId, int subId, void *arg)
+{
+	int			i;
+	const FmgrBuiltin *entry;
+
+	if (ddl_guard_enabled && !superuser())
+	{
+		if (ddl_guard_lo_sentinel)
+		{
+			switch (access)
+			{
+				case OAT_FUNCTION_EXECUTE:
+					/* look up the func and log that shit */
+					if (objectId >= min_reserved_oid && objectId <= max_reserved_oid)
+					{
+						for (i = 0; i < lobject_funcs_count; i++)
+						{
+							if (lobject_func_oids[i] == objectId)
+							{
+								/* ooh that's a bingo! */
+								if ((entry = fmgr_lookupByName(large_object_funcs[i])) != NULL)
+								{
+									write_sentinel_file(LO_SENTINEL_FILE);
+									ereport(WARNING, errmsg("lo_guard: lobject write function call, sentinel file written"));
+								}
+							}
+						}
+					}
+				default:
+					break;
+			}
+			if (classId == LargeObjectRelationId)
+			{
+				switch (access)
+				{
+					case OAT_POST_CREATE:
+						write_sentinel_file(LO_SENTINEL_FILE);
+						ereport(WARNING, (errmsg("ddl_guard: pg_largeobject creation detected, sentinel file written")));
+						break;
+					case OAT_DROP:
+						write_sentinel_file(LO_SENTINEL_FILE);
+						ereport(WARNING, (errmsg("ddl_guard: pg_largeobject deletion detected, sentinel file written")));
+						break;
+					default:
+						break;
+				}
+			}
+		}
+	}
+	if (next_object_access_hook)
+		(*next_object_access_hook) (access, classId, objectId, subId, arg);
 }
 
 Datum
@@ -95,4 +197,46 @@ ddl_guard_check(PG_FUNCTION_ARGS)
 	};
 
 	PG_RETURN_VOID();
+}
+
+void
+_PG_init(void)
+{
+	DefineCustomBoolVariable("ddl_guard.enabled", "Enable or disable DDL Guard",
+							 NULL, &ddl_guard_enabled, false, PGC_SUSET, 0, NULL,
+							 NULL, NULL);
+
+	DefineCustomBoolVariable("ddl_guard.ddl_sentinel", "Write sentinel file for DDL statements",
+							 NULL, &ddl_guard_ddl_sentinel, false, PGC_SUSET, 0, NULL,
+							 NULL, NULL);
+
+	DefineCustomBoolVariable("ddl_guard.lo_sentinel", "Write sentinel file for pg_largeobject modifications",
+							 NULL, &ddl_guard_lo_sentinel, false, PGC_SUSET, 0, NULL,
+							 NULL, NULL);
+
+	unlink(DDL_SENTINEL_FILE);
+	unlink(LO_SENTINEL_FILE);
+
+	if (set_lobject_func_oids())
+	{
+		next_object_access_hook = object_access_hook;
+		object_access_hook = lob_object_access_hook;
+	}
+	else
+	{
+		ereport(ERROR, errmsg("We beefed it, chief."));
+	}
+
+	EmitWarningsOnPlaceholders("ddl_guard");
+}
+
+void
+_PG_fini(void)
+{
+	if (lobject_func_oids != NULL)
+	{
+		free(lobject_func_oids);
+	}
+
+	object_access_hook = next_object_access_hook;
 }
